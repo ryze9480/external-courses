590f8f2056c26ff8291672a3568be7d75c8fe573
ryze9480 <ryze9480@yandex.ru>
2020-03-14 14:41:54 +0300

    task 2

---
+++ src/ex2_js-basics-part2/task-02.js
@@ -0,0 +1,12 @@
+function logElements(arr){
+  for(i=0;i<arr.length;i++){
+    console.log(arr[i])  
+  }
+  console.log(arr.length)
+    return
+  }
+  
+  module.exports=logElements
+ 
+      
+   
