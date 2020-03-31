421946f1fd683279bd6307196626c5f8e006141f
ryze9480 <ryze9480@yandex.ru>
2020-03-31 15:53:01 +0300

    task11

---
+++ src/ex4_js-objects-part2/task-11.js
@@ -0,0 +1,9 @@
+function roundingANumber(number1,number2){
+if(typeof (number1&&number2)==="number"&&Number.isNaN(number1&&number2)!=="NaN"){
+const result=number1+number2;
+ }
+
+   return result.toFixed(3);
+}
+
+module.exports=roundingANumber
