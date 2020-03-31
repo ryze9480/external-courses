89acee5fea97b1314593ae61b75146e2744b1395
ryze9480 <ryze9480@yandex.ru>
2020-03-31 17:11:23 +0300

    task13

--- src/ex4_js-objects-part2/task-12.js
+++
@@ -1,8 +0,0 @@
-function roundingANumber(number1,number2){
-if(typeof (number1&&number2)==="number"&&Number.isNaN(number1&&number2)!=="NaN"){
-const result=number1+number2;
- }
-return result.toFixed(3);
-}
-
-module.exports=roundingANumber
---
+++ src/ex4_js-objects-part2/task-13.js
@@ -0,0 +1,6 @@
+function getRandomNumber(){
+   return Math.floor(Math.random()*101)
+ 
+}
+
+module.exports=getRandomNumber
